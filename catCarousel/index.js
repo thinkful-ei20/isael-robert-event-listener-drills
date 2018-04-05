30b31a543a82f942657405793a3edd8975cd0db8
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-05 12:23:00 -0600

    finished etcher

--- catCarousel/index.js
+++ catCarousel/index.js
@@ -3,7 +3,10 @@ function main(){
   $('.thumbnail').on('click', function (){
     const source = $(this).children('img').attr('src');
     const alt = $(this).children('img').attr('alt');
-    $('.hero').children('img').attr('src', source).attr('alt', alt);
+    $('.hero')
+      .children('img')
+      .attr('src', source)
+      .attr('alt', alt);
   });
 }
 $(main);
